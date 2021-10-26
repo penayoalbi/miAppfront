f40822bab896e05b0034a4b563b1b2d1d4c549d3
albi.penayo <albi.penayo@gmail.com>
2021-10-26 15:51:56 -0300

    mensajes y usuarios

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Usuario } from 'src/app/clase/usuario';
 import { ApiServiceService } from 'src/app/helper/api-service.service';
 import { UsuarioServiceService } from 'src/app/helper/usuario-service.service';
+import { MensajeComponent } from '../mensaje/mensaje.component';
 
 @Component({
   selector: 'app-login',
@@ -10,19 +12,32 @@ import { UsuarioServiceService } from 'src/app/helper/usuario-service.service';
 })
 export class LoginComponent implements OnInit {
   usuario: Usuario;
-  constructor(private apiService: ApiServiceService, private userService: UsuarioServiceService) {
-    this.usuario= new Usuario;
+
+  constructor(
+    private apiService: ApiServiceService,
+    private userService: UsuarioServiceService,
+    private ruteo: Router) {
+    this.usuario = new Usuario;
    }
 
   ngOnInit(): void {
   }
 
+  logeo(resp: Usuario[]){
+    if(resp.length==0){
+      alert("Error: usuario no valido");
+    }else{
+      this.userService.setUsuario(resp[0]);
+      this.ruteo.navigateByUrl('usuarios');
+    }
+  }
+
   login(){
     this.apiService.ingresar(this.usuario).subscribe(
-      datos=>{
-        this.userService.setUsuario(datos[0]);
-      }
+      datos => {
+      this.logeo(datos);}, err=>alert(err.message)
     );
   }
+  
 }
 
--- src/app/componentes/usuarios/usuarios.component.ts
+++ src/app/componentes/usuarios/usuarios.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { useAnimation } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
+import { Mensaje } from 'src/app/clase/mensaje';
+import { Usuario } from 'src/app/clase/usuario';
+import { ApiServiceService } from 'src/app/helper/api-service.service';
+import { UsuarioServiceService } from 'src/app/helper/usuario-service.service';
 
 @Component({
   selector: 'app-usuarios',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent implements OnInit {
+  mensaje!: string;
+  public usuarios!: Usuario[];
+  public mensajes: Mensaje[];
+  public usuarioActual!:Usuario;
+  mensajeNuevo: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  setearUsuario(seleccionado: Usuario){
+    this.usuarioActual = seleccionado;
+    this.apiService.traerMensaje(this.usuarioService.getUsuario().id, this.usuarioService.getUsuario().token,seleccionado.id).subscribe(
+      then => this.mensajes = then);
   }
 
+  constructor(private apiService: ApiServiceService, private usuarioService: UsuarioServiceService) {
+    this.mensajeNuevo = false;
+    this.apiService.listaUsuario(this.usuarioService.getUsuario().id, this.usuarioService.getUsuario().token).subscribe(
+      then => this.usuarios = then);
+    this.mensaje =this.mensaje;
+    this.mensajes=[];
+  }
+  ngOnInit(): void {
+  }
+  agregarNuevo(){
+    this.mensajeNuevo = true;
+  }
+
+  enviar(){
+    this.apiService.crearMensaje(this.mensaje, this.usuarioActual.id, this.usuarioService.getUsuario().id,
+    this.usuarioService.getUsuario().token).subscribe(then => this.setearUsuario(this.usuarioActual));
+    this.mensajeNuevo = false;
+  }
 }
